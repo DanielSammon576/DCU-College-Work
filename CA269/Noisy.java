3fe93fd41eea1731845ce720d1d54c4f95acef4f
Daniel Sammon <daniel.sammon4@mail.dcu.ie>
2020-11-11 12:29:11 +0000

    Removing

--- CA269/Noisy.java
+++
@@ -1,8 +0,0 @@
-public class Noisy
-{
-    public static Animal [] makeSomeNoise()
-    {
-        Animal [] animals = {new Dog("Devil"), new Cat("Angel"), new Cat("Cheesire"), new Dog("Buster"), new Dog("Fido"), new Cat("Lassie")};
-        return animals;
-    }
-}
